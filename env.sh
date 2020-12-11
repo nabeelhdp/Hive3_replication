1426779e3a4fa238608b57800dd5f80941ea7d01
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2020-12-11 14:15:04 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -8,11 +8,11 @@ source_jdbc_url="jdbc:hive2://c2186-node2.coelab.cloudera.com:2181,c2186-node3.c
 # Name of database to be synced.
 # This will be overridden if the script is invoked with a database name as argument.
 dbname="repltest"
-# For input sanity check 
-# List of acceptable dbnames (inclusive of both source and target) when passed via argument to script.
-dblist="repltest employee"
+# List of acceptable dbnames when passed via argument to script. Sanity check.
+dblist="repltest repltest_replica"
 
 repl_root="/apps/hive/repl"
+#source_hdfs_prefix="hdfs://c2186"
 source_hdfs_prefix="hdfs://c2186-node2.coelab.cloudera.com:8020"
 beeline_opts="--verbose=false --showHeader=false --silent=true"
 beeline_user="hive"
@@ -20,7 +20,7 @@ repl_log_file="./replication.log"
 
 # Locations for the various Hive QL scripts for each action.
 HQL_DIR="./HQL"
-BOOTSTRAP_HQL="${HQL_DIR}/repldump.hql"
-INC_DUMP_HQL="${HQL_DIR}/replbootstrap.hql"
+INC_DUMP_HQL="${HQL_DIR}/repldump.hql"
+BOOTSTRAP_HQL="${HQL_DIR}/replbootstrap.hql"
 LOAD_HQL="${HQL_DIR}/replload.hql"
 STATUS_HQL="${HQL_DIR}/replstatus.hql"
