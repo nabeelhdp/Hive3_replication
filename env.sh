6d865043baf5b6c02d8c4d0680d8bef96b442887
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2020-12-18 14:24:44 +0800

    Update env.sh
    Removing the option of a different target database name as it breaks view compatibility

--- env.sh
+++ env.sh
@@ -4,13 +4,6 @@
 target_jdbc_url="jdbc:hive2://c4186-node2.coelab.cloudera.com:2181,c4186-node3.coelab.cloudera.com:2181,c4186-node4.coelab.cloudera.com:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2"
 source_jdbc_url="jdbc:hive2://c2186-node2.coelab.cloudera.com:2181,c2186-node3.coelab.cloudera.com:2181,c2186-node4.coelab.cloudera.com:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2"
 
-
-# Name of database to be synced from.
-dbname="repltest"
-# Name of database to be synced to.
-# This will be overridden if the script is invoked with a database name as argument.
-targetdbname="repltest_replica"
-
 # List of acceptable dbnames when passed via argument to script. This is for a sanity check to avoid accidental full dump generation in prod for mistyped target database names.
 dblist="repltest repltest_replica"
 
