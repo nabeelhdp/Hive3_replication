9cdc08577d072bde3eafcffe5b977ed0471fd6ee
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2020-12-31 12:19:15 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -7,6 +7,7 @@ source_jdbc_url="jdbc:hive2://c2186-node2.coelab.cloudera.com:2181,c2186-node3.c
 # List of acceptable dbnames when passed via argument to script. This is for a sanity check to avoid accidental full dump generation in prod for mistyped target database names.
 dblist="repltest"
 
+# The default behaviour of the REPL command excludes external tables. Set this to true to include external tables in the replication process.
 include_external_tables=false
 
 # location in source hdfs where dump data will be written. This is used only to verify REPL DUMP output starting suffix
@@ -16,9 +17,12 @@ repl_root="/apps/hive/repl"
 #source_hdfs_prefix="hdfs://c2186"
 source_hdfs_prefix="hdfs://c2186-node2.coelab.cloudera.com:8020"
 
+# User running beeline. In kerberized environments this may be ignored.
 beeline_user="hive"
 
+# This location can be changed if needed.
+LOG_DIR="./logs"
+
+# Set unique value for logs and output directory based on time of script run
 current_time=$(date +"%Y_%m_%d_%I_%M_%p")
 TMP_DIR="./tmp/run_${current_time}"
-LOG_DIR="./logs"
-HQL_DIR="./HQL"
