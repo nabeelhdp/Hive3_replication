a0beae238fed0f69ffb30f7b3aaef226e1730a83
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2021-01-25 22:35:44 +0800

    Updated variable case
    TARGET_JDBC_URL
    SOURCE_JDBC_URL
    SOURCE_HDFS_PREFIX
    REPL_ROOT
    INCLUDE_EXTERNAL_TABLES

--- env.sh
+++ env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # JDBC URLs from both clusters. Copy this value from the Ambari UI.
-target_jdbc_url="jdbc:hive2://c4186-node2.coelab.cloudera.com:2181,c4186-node3.coelab.cloudera.com:2181,c4186-node4.coelab.cloudera.com:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2"
-source_jdbc_url="jdbc:hive2://c2186-node2.coelab.cloudera.com:2181,c2186-node3.coelab.cloudera.com:2181,c2186-node4.coelab.cloudera.com:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2"
+TARGET_JDBC_URL="jdbc:hive2://c4186-node2.coelab.cloudera.com:2181,c4186-node3.coelab.cloudera.com:2181,c4186-node4.coelab.cloudera.com:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2"
+SOURCE_JDBC_URL="jdbc:hive2://c2186-node2.coelab.cloudera.com:2181,c2186-node3.coelab.cloudera.com:2181,c2186-node4.coelab.cloudera.com:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2"
 
 # The default behaviour of the REPL command excludes external tables. Set this to true to include external tables in the replication process.
-include_external_tables=false   # [true|false]
+INCLUDE_EXTERNAL_TABLES=false   # [true|false]
 
 # location in source hdfs where dump data will be written. This is used only to verify REPL DUMP output starting suffix
-repl_root="/apps/hive/repl"
+REPL_ROOT="/apps/hive/repl"
 
 # Prefix to access HDFS locations at source cluster as accessed from target
-#source_hdfs_prefix="hdfs://c2186"
-source_hdfs_prefix="hdfs://c2186-node2.coelab.cloudera.com:8020"
+#SOURCE_HDFS_PREFIX="hdfs://c2186"
+SOURCE_HDFS_PREFIX="hdfs://c2186-node2.coelab.cloudera.com:8020"
 
 # User running beeline. In kerberized environments this may be ignored.
 beeline_user="hive"
