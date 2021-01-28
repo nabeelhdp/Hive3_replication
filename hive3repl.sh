d98f13e6ebb6814020ec59931df1d4539b025f73
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2021-01-28 13:02:32 +0800

    Added LOG TYPE to messages

--- hive3repl.sh
+++ hive3repl.sh
@@ -64,7 +64,7 @@ trap ' trap_log_int ${lock_name} ' INT TERM
 trap ' trap_log_exit ${lock_name} ' EXIT 
 
 echo "===================================================================" >>${REPL_LOG_FILE}
-printmessage "Initiating run to replicate ${DBNAME} to ${DBNAME}."
+printmessage " INFO: Initiating run to replicate ${DBNAME} to ${DBNAME}."
 echo "==================================================================="  >>${REPL_LOG_FILE}
 echo "For detailed logging, run tail -f on ${REPL_LOG_FILE}"
 
@@ -74,29 +74,29 @@ retrieve_current_target_repl_id
 # If the REPL STATUS output is "NULL", it means the database has not been replicated.
 # So a full dump will be generated.
 if [[ "${last_repl_id}" == "NULL" ]]; then
-  printmessage "No replication id detected at target. Full data dump dump needs to be initiated."
-  printmessage "Database ${DBNAME} is being synced for the first time. Initiating full dump."
+  printmessage " INFO: No replication id detected at target. Full data dump dump needs to be initiated."
+  printmessage " INFO: Database ${DBNAME} is being synced for the first time. Initiating full dump."
   
   # Generate bootstrap dump at source
   gen_bootstrap_dump_source
   
   # dump_txid is set in the above function to the current transaction ID at source cluster for the database
   source_latest_txid=${dump_txid}
-  printmessage "Source transaction id: |${source_latest_txid}|"
+  printmessage " INFO: Source transaction id: |${source_latest_txid}|"
 
   # If dump is generated successfully, a proper integer value is returned.
   if [[ "${source_latest_txid}" -gt 0 ]]; then
-    printmessage "Database ${DBNAME} full dump has been generated at |${SOURCE_HDFS_PREFIX}${dump_path}|."
-    printmessage "The current transaction ID at source is |${source_latest_txid}|"
-    printmessage "There are ${source_latest_txid} transactions to be synced in this run."
-    printmessage "Initiating data load at target cluster on database ${DBNAME}."
+    printmessage " INFO: Database ${DBNAME} full dump has been generated at |${SOURCE_HDFS_PREFIX}${dump_path}|."
+    printmessage " INFO: The current transaction ID at source is |${source_latest_txid}|"
+    printmessage " INFO: There are ${source_latest_txid} transactions to be synced in this run."
+    printmessage " INFO: Initiating data load at target cluster on database ${DBNAME}."
 
     # Point to corresponding HQL file depending on whether external tables are to be included or not
     if [[ "${INCLUDE_EXTERNAL_TABLES}" == "true" ]]; then
-      printmessage "External tables included. This may trigger distcp jobs in background."
+      printmessage " INFO: External tables included. This may trigger distcp jobs in background."
       HQL_FILE=${EXT_LOAD_HQL}
     else 
-      printmessage "External tables not included."
+      printmessage " INFO: External tables not included."
       HQL_FILE=${LOAD_HQL}
     fi 
     
@@ -105,26 +105,26 @@ if [[ "${last_repl_id}" == "NULL" ]]; then
     # Now the source cluster has generated a full dump of the database. 
     # Replay the full dump on the target cluster database.
     if replay_dump_at_target ${HQL_FILE}; then 
-      printmessage "Data load at target cluster completed. Verifying...." 
+      printmessage " INFO: Data load at target cluster completed. Verifying...." 
       retrieve_post_load_target_repl_id
       if [[ "${post_load_repl_id}" == "${source_latest_txid}" ]] ; then
-        printmessage "Database replication completed SUCCESSFULLY. Last transaction id at target is |${post_load_repl_id}|"
+        printmessage " INFO: Database replication completed SUCCESSFULLY. Last transaction id at target is |${post_load_repl_id}|"
       elif [[ "${post_load_repl_id}" == "NULL" ]] ; then
-        printmessage "Database replication FAILED. No transactions have been applied in this run."
+        printmessage " ERROR: Database replication FAILED. No transactions have been applied in this run."
         # TODO - cleanup directories leftover during failed replication (if any) 
      elif [[ ${post_load_repl_id} -lt ${source_latest_txid} ]] ; then
-        printmessage "ERROR: Transaction event ID in target is behind the event ID at source at time of current dump."
-        printmessage "ERROR: This will require a cleanup of the partially loaded database in target."
+        printmessage " ERROR: Transaction event ID in target is behind the event ID at source at time of current dump."
+        printmessage " ERROR: This will require a cleanup of the partially loaded database in target."
      elif [[ ${post_load_repl_id} -gt ${source_latest_txid} ]] ; then
-        printmessage "Transaction event ID in target is ahead of the event ID at source at time of current dump."
-        printmessage "This may happen if there is another REPL LOAD in progress with a later copy of the source dump"
+        printmessage " WARN: Transaction event ID in target is ahead of the event ID at source at time of current dump."
+        printmessage " WARN: This may happen if there is another REPL LOAD in progress with a later copy of the source dump"
      else
-        printmessage "Unable to verify database replication! Post Load repl id: |${post_load_repl_id}|"
-        printmessage "Source repl id: |${source_latest_txid}|"    
+        printmessage " ERROR: Unable to verify database replication! Post Load repl id: |${post_load_repl_id}|"
+        printmessage " ERROR: Source repl id: |${source_latest_txid}|"    
         exit 1
       fi
     else 
-      printmessage "Data load at target cluster failed" 
+      printmessage " ERROR: Data load at target cluster failed" 
       echo -e "See ${REPL_LOG_FILE} for details. Exiting!" 
       exit 1
     fi 
@@ -132,71 +132,71 @@ if [[ "${last_repl_id}" == "NULL" ]]; then
   # If source_latest_txid is anything but a proper number, 
   # it indicates a failure in geenerating the source dump. Exit.
   else
-    printmessage "Unable to generate full dump for database ${DBNAME}. Exiting!."
+    printmessage " ERROR: Unable to generate full dump for database ${DBNAME}. Exiting!."
     exit 1
   fi
 
 # If the database at target cluster already has some transaction replayed, 
 # trigger the source dump as an incremental dump.
 elif [[ ${last_repl_id} =~ ${re} ]] ; then
-  printmessage "Database ${DBNAME} transaction ID at target is currently |${last_repl_id}|"
+  printmessage " INFO: Database ${DBNAME} transaction ID at target is currently |${last_repl_id}|"
   
   # Generate incremental dump at source
   gen_incremental_dump_source
  
   # dump_txid is set in the above function to the current transaction ID at source cluster for the database
   source_latest_txid=${dump_txid}
-  printmessage "The current transaction ID at source is |${source_latest_txid}|"
+  printmessage " INFO: The current transaction ID at source is |${source_latest_txid}|"
 
   if [[ "${source_latest_txid}" -gt 0 ]]; then
-    printmessage "Database ${DBNAME} incremental dump has been generated at |${SOURCE_HDFS_PREFIX}${dump_path}|."
+    printmessage " INFO: Database ${DBNAME} incremental dump has been generated at |${SOURCE_HDFS_PREFIX}${dump_path}|."
     # the calculation of txn_count below doesn't match with numEvents that show up in the 
     #txn_count=$((${source_latest_txid} - ${last_repl_id}))
-    printmessage "Initiating REPL LOAD at destination cluster to replicate ${DBNAME} to transaction id |${source_latest_txid}|."
+    printmessage " INFO: Initiating REPL LOAD at destination cluster to replicate ${DBNAME} to transaction id |${source_latest_txid}|."
     
     # Point to corresponding HQL file depending on whether external tables are to be included or not
     if [[ "${INCLUDE_EXTERNAL_TABLES}" == "true" ]]; then
-      printmessage "External tables included. This may trigger distcp jobs in background."
+      printmessage " INFO: External tables included. This may trigger distcp jobs in background."
       HQL_FILE=${EXT_LOAD_HQL}
     else 
-      printmessage "External tables not included."
+      printmessage " INFO: External tables not included."
       HQL_FILE=${LOAD_HQL}
     fi 
         
     # Now the source cluster has generated a dump of the database. 
     # Replay the dump on the target cluster database.
     if replay_dump_at_target ${HQL_FILE}; then 
-      printmessage "Data load at target cluster completed. Verifying...." 
+      printmessage " INFO: Data load at target cluster completed. Verifying...." 
       retrieve_post_load_target_repl_id
       if [[ "${post_load_repl_id}" == "${source_latest_txid}" ]] ; then
-        printmessage "Database replication completed SUCCESSFULLY. Last transaction id at target is |${post_load_repl_id}|"
+        printmessage " INFO: Database replication completed SUCCESSFULLY. Last transaction id at target is |${post_load_repl_id}|"
       elif [[ "${post_load_repl_id}" == "${last_repl_id}" ]] ; then
-        printmessage "Database replication FAILED. No transactions have been applied in this run."
+        printmessage " ERROR: Database replication FAILED. No transactions have been applied in this run."
       elif [[ ${post_load_repl_id} -lt ${source_latest_txid} ]] ; then
-        printmessage "WARN: Transaction event ID in target is behind the event ID at source at time of current dump."
-        printmessage "INFO: This can be fixed by a rerun of the script."
+        printmessage " WARN: Replication from source to target is incomplete."
+        printmessage " WARN: Transaction event ID in target is behind the event ID at source at time of current dump."
      elif [[ ${post_load_repl_id} -gt ${source_latest_txid} ]] ; then
-        printmessage "Transaction event ID in target is ahead of the event ID at source at time of current dump."
-        printmessage "This may happen if there is another REPL LOAD in progress with a later copy of the source dump"
+        printmessage " WARN: Transaction event ID in target is ahead of the event ID at source at time of current dump."
+        printmessage " WARN: This may happen if there is another REPL LOAD in progress with a later copy of the source dump"
       else
-        printmessage "Unable to verify database replication! Post Load repl id: |${post_load_repl_id}|"
-        printmessage "Source repl id: |${source_latest_txid}|"    
+        printmessage " ERROR: Unable to verify database replication! Post Load repl id: |${post_load_repl_id}|"
+        printmessage " ERROR: Source repl id: |${source_latest_txid}|"    
         exit 1
       fi
     else 
-      printmessage "Data load at target cluster failed" 
+      printmessage " ERROR: Data load at target cluster failed" 
       echo -e "See ${REPL_LOG_FILE} for details. Exiting!" 
       exit 1
     fi 
         
   else
-    printmessage "Invalid transaction id returned from Source : |${source_latest_txid}|"
-    printmessage "Unable to generate incremental dump for database ${DBNAME}. Exiting!."
+    printmessage " ERROR: Invalid transaction id returned from Source : |${source_latest_txid}|"
+    printmessage " ERROR: Unable to generate incremental dump for database ${DBNAME}. Exiting!."
     exit 1
   fi
 
 else
-  printmessage "Invalid value for last replicated transaction id: ${last_repl_id}. Database dump failed"
+  printmessage " ERROR: Invalid value for last replicated transaction id: ${last_repl_id}. Database dump failed"
   echo -e "See ${REPL_LOG_FILE} for details. Exiting!"
   exit 1
 fi
